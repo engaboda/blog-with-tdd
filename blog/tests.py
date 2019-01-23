97f9fb77bb1dc65dcdc3fe001e4e07faaffeef48
Abdalla salah elkasass <abodaelkaass@yahoo.com>
2019-01-23 15:35:56 +0200

    second commit

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+# from djago.utils import datetime
 
 from .models import Entry
+from .models import Comment
 
+from .forms import CommentForm
 
+from django_webtest import WebTest
 
 class EntryModelTest(TestCase):
 
@@ -44,7 +48,7 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertContains(response, 'no blog entries')
 
-class EntryViewTest(TestCase):
+class EntryViewTest(WebTest):
     def setUp(self):
         self.user = get_user_model().objects.create(username='aboda')
         self.entry = Entry.objects.create(title='1-title', body='1-body', author=self.user )
@@ -65,3 +69,89 @@ class EntryViewTest(TestCase):
     def test_body_in_entry(self):
         response = self.client.get(self.entry.get_absolute_url())
         self.assertContains(response, self.entry.body)
+
+    def test_view_page(self):
+        page = self.app.get(self.entry.get_absolute_url())
+        self.assertEqual(len(page.forms),1)
+
+
+    def test_form_error(self):
+        page = self.app.get(self.entry.get_absolute_url())
+        page = page.form.submit()
+        self.assertContains( page, 'This field is required.')
+
+    def test_from_success(self):
+        page = self.app.get( self.entry.get_absolute_url() )
+        page.form['name'] ='aboda'
+        page.form['email'] ='abodaelkaass@yahoo.com'
+        page.form['body'] ='i love test'
+        page = page.form.submit()
+        self.assertRedirects( page, self.entry.get_absolute_url())
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='aboda')
+        self.entry = Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        self.comment = Comment.objects.create(
+            entry=self.entry,
+            name='aboda',
+            email='aboda@yahoo.com',
+            body='i love coding',
+            )
+    
+    def test_string_representation(self):
+        self.assertEqual(str(self.comment), 'i love coding' )
+    
+    def test_comment_in_entry_detail(self):
+        comment = Comment.objects.create(
+            entry=self.entry,
+            name='aboda',
+            email='aboda@yahoo.com',
+            body='i love coding',
+        )
+        response = self.client.get(self.entry.get_absolute_url())
+        self.assertContains( response, 'i love coding' )
+    
+    def test_empty_comment(self):
+        self.entry = Entry.objects.create(title='1-title', body='1-body', author=self.user)
+        response = self.client.get( self.entry.get_absolute_url() )
+        self.assertContains( response, 'No comments yet.' )
+
+
+class CommentFormTest(TestCase):
+    def setUp(self):
+        user = get_user_model().objects.create(username='ahmed')
+        self.entry = Entry.objects.create(author=user, title='1-title')
+
+    def test_init(self):
+        CommentForm(entry=self.entry)
+
+    def test_form_without_entry(self):
+        with self.assertRaises(KeyError):
+            CommentForm()
+
+    def test_valid_data(self):
+        form = CommentForm({
+            'name':'aboda elkasass',
+            'email':'abodaelkaass@yahoo.com',
+            'body':'i love coding',
+        }, entry=self.entry) 
+
+        self.assertTrue(form.is_valid())
+        comment = form.save()
+        self.assertEqual( comment.name,'aboda elkasass' )
+        self.assertEqual( comment.email,'abodaelkaass@yahoo.com' )
+        self.assertEqual( comment.body,'i love coding' )
+    
+    def test_blank_data(self):
+        form = CommentForm( {}, entry=self.entry )
+        self.assertFalse( form.is_valid() )
+        self.assertEqual( form.errors, {
+            'name':['This field is required.'],
+            'email':['This field is required.'],
+            'body':['This field is required.'],
+        } )
+
+
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
-from django.views.generic import DeleteView
+from django.views.generic import CreateView
+from django.shortcuts import get_object_or_404
+
 from .models import Entry
+
+from .forms import CommentForm
 
 # Create your views here.
 
-class EntryDetail(DeleteView):
+class EntryDetail(CreateView):
     model = Entry
     template_name = 'blog/entry_detail.html'
+    form_class = CommentForm
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['entry'] = self.get_object()
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['entry'] = self.get_object()
+        return context
+    def get_success_url(self):
+        return self.get_object().get_absolute_url()
